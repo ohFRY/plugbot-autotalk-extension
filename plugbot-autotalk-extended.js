b49a4e8669729a84f36833fdaecf8b985b4b9485
Fry Falavel <fry@justgomusic.com>
2013-03-20 23:19:30 +0000

    quick fix for greeting message

--- plugbot-autotalk-extended.js
+++ plugbot-autotalk-extended.js
@@ -125,7 +125,7 @@ function initAPIListeners() {
             populateUserlist();
         }
         var temp = new Date().getTime();
-        if(temp > (timeout+TIME_RANGE_AUTOTALK) {
+        if(temp > (timeout+TIME_RANGE_AUTOTALK)) {
             API.sendChat('Hey @' + user.username);
             timeout = temp;
         }
